52d52ee69df31be5752ca1412f4b820aca936a5f
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-12-01 17:02:45 +0300

    Add function add_random_spot

--- defects.py
+++ defects.py
@@ -11,6 +11,13 @@ def add_spot(img, spot_bbox, color):
     return img
 
 
+def add_random_spot(img, seed=None):
+    spot_bbox = get_random_ellipse(img.size, seed)
+    color = get_random_color(seed)
+    spoted_image = add_spot(img, spot_bbox, color)
+    return spoted_image
+
+
 if __name__ == '__main__':
     img = Image.open('E:\\Projects\\image-repairing\\ImageNet2012\\test\\ILSVRC2012_test_00000002.JPEG').convert('RGB')
     bbox = get_random_ellipse(img.size)
