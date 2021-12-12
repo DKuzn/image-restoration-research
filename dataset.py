506fd210c80d6a9c6ed3c4cb531a57c070e29585
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-12-12 19:53:16 +0300

    Update dataset class

--- dataset.py
+++ dataset.py
@@ -1,34 +1,36 @@
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
-import numpy as np
 import random as rd
 import os
-from defects import add_random_spot
+import copy
+from utils import image_to_tensor
+from typing import Callable
 
 
 class ImageNet2012(Dataset):
-    def __init__(self, root: str, paths_list: str):
+    def __init__(self, root: str, paths_list: str, transform: Callable = None, size: int = None, transform_kwargs = {}):
         self.root = root
         self.paths = self._list_paths(paths_list)
+        self.transform = transform
+        self.size = size
+        self.kwargs = transform_kwargs
 
     def __len__(self):
         return len(self.paths)
 
     def __getitem__(self, item):
-        path = f'{self.root}{os.sep}' + self.paths[item].replace('/', os.sep).split(' ')[0] + '.JPEG'
+        ds_path = self.paths[item].replace('/', os.sep).split(' ')[0]
+        path = f'{self.root}{os.sep}{ds_path}.JPEG'
         image = Image.open(path).convert('RGB')
-        image = image.resize((256, 256))
-        y_image = image
-        for _ in range(rd.randint(0, 10)):
-            y_image = add_random_spot(y_image)
-        image = np.array(image)
-        image = np.rollaxis(image, 2)
-        y_image = np.array(y_image)
-        y_image = np.rollaxis(y_image, 2)
-        x = torch.Tensor(image / 255.0)
-        y = torch.Tensor(y_image / 255.0)
-        return y, x
+        if self.size is not None:
+            image = image.resize((self.size, self.size))
+        y_image = copy.deepcopy(image)
+        if self.transform is not None:
+            image = self.transform(image, **self.kwargs)
+        x = image_to_tensor(image)
+        y = image_to_tensor(y_image)
+        return x, y
 
     def _list_paths(self, paths_list: str):
         paths = open(paths_list, 'r').read().split('\n')
