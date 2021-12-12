4c1ba86f0972fbf727d169c726851a09ffc4737e
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-12-12 19:46:53 +0300

    Add new utils

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import random as rd
+import numpy as np
+import torch
 
 
 def get_random_color(seed=None):
@@ -22,3 +24,27 @@ def get_random_ellipse(img_size, seed=None):
     ellipse.append(rd.randint(ellipse[1], img_size[1]))
 
     return ellipse
+
+
+def get_random_scratch(img_size, seed=None):
+    if seed is not None:
+        rd.seed(seed)
+
+    line = []
+    for _ in range(2):
+        line.append(rd.randint(0, img_size[0]))
+        line.append(rd.randint(0, img_size[1]))
+
+    return line
+
+
+def tensor_to_image(tensor):
+    img = tensor.detach().numpy()
+    img = np.moveaxis(img, 0, -1)
+    return img
+
+
+def image_to_tensor(image):
+    image = np.array(image)
+    image = np.moveaxis(image, -1, 0)
+    return torch.Tensor(image / 255.0)
